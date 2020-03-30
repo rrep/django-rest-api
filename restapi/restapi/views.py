92d2c15866997848dada33c9c689eb5b571f8346
rrep <pwduerr@gmail.com>
2020-03-29 18:17:01 -0600

    initial rest framework setup

---
+++ restapi/restapi/views.py
@@ -0,0 +1,8 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET'])
+def index(request):
+    message = 'hello from the apiiiiiiiiiiiii'
+    return Response(data=message, status=status.HTTP_200_OK)
